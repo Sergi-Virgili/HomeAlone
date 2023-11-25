bdd85d390b23ada65a164ffe37d8477c29b967a9
Sergi-Virgili <svirgilif@gmail.com>
2023-11-25 10:20:29 +0100

    Activate, desactivate alarm and motionSensor Fake

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-//
+
 class Updater
 {
 protected:
@@ -7,19 +7,17 @@ protected:
   const unsigned long interval;
 
 public:
-  Updater(unsigned long interval) : previousMillis(0), interval(interval) {}
+  Updater(unsigned long interval) : interval(interval), previousMillis(0) {}
 
-  virtual void update()
+  void update()
   {
     unsigned long currentMillis = millis();
     if (currentMillis - previousMillis >= interval)
     {
       previousMillis = currentMillis;
-      performUpdate(); // Llama a la función que se debe implementar en la clase derivada
+      performUpdate();
     }
   }
-
-  // Función virtual pura que las clases derivadas deben implementar
   virtual void performUpdate() = 0;
 };
 
@@ -27,17 +25,36 @@ class LEDBlinker : public Updater
 {
 private:
   int ledPin;
+  String mode = "OFF";
 
 public:
   LEDBlinker(int pin, unsigned long interval) : Updater(interval), ledPin(pin)
   {
     pinMode(ledPin, OUTPUT);
-  }
+  };
 
   // Implementación específica de la actualización para el LEDBlinker
   void performUpdate() override
   {
-    digitalWrite(ledPin, !digitalRead(ledPin)); // Cambia el estado del LED
+    if (mode == "OFF")
+    {
+      digitalWrite(ledPin, LOW);
+      return;
+    }
+    if (mode == "ON")
+    {
+      digitalWrite(ledPin, HIGH);
+      return;
+    }
+    if (mode == "BLINK")
+    {
+      digitalWrite(ledPin, !digitalRead(ledPin)); // Cambia el estado del LED
+      return;
+    }
+  }
+  void changeMode(String newMode)
+  {
+    mode = newMode;
   }
 };
 
@@ -47,7 +64,7 @@ class Alarm : public Updater
 {
 private:
   bool alarmState;
-  bool isActivated = true;
+  bool isActivated = false;
 
 public:
   Alarm(unsigned long interval) : Updater(interval), alarmState(false) {}
@@ -64,13 +81,31 @@ public:
 
   void alarm()
   {
-    if (!isActivated)
+    if (isActivated == false)
       return;
     if (!alarmState)
     {
+      Serial.println("HA ENTRADO ALGUIEN");
       Serial.println("ALARMA SONANDO - EVIA UN EMAIL");
+      alarmState = true;
+      return;
     }
     alarmState = true;
+    Serial.println("ALARMA SONANDO");
+  }
+  void deactivate()
+  {
+    isActivated = false;
+    Serial.println("DESACTIVAR");
+  }
+  void activate()
+  {
+    isActivated = true;
+    Serial.println("ACTIVAR ALARMA");
+  }
+  bool isActivatedAlarm()
+  {
+    return isActivated;
   }
 };
 
@@ -79,45 +114,86 @@ public:
 class MotionSensor : public Updater
 {
 private:
+  bool isActivated = false;
   int motionSensorPin;
   bool motionState;
 
 public:
-  MotionSensor(int pin, unsigned long interval) : Updater(interval), motionSensorPin(pin), motionState(true)
+  MotionSensor(int pin, unsigned long interval) : Updater(interval), motionSensorPin(pin), motionState(false)
   {
     pinMode(motionSensorPin, INPUT);
   }
 
   void performUpdate() override
   {
-    motionState = digitalRead(motionSensorPin);
+    setMotionState(!getMotionState());
   }
 
   bool getMotionState()
   {
     return motionState;
   }
+  void setMotionState(bool state)
+  {
+    motionState = state;
+    if (motionState)
+      Serial.println("DETECTADO MOVIMIENTO");
+    if (!motionState)
+      Serial.println("NO HAY MOVIMIENTO");
+  }
+  void activate()
+  {
+    isActivated = true;
+    Serial.println("ACTIVAR SENSOR MOVIMIENTO");
+  }
+  void deactivate()
+  {
+    isActivated = false;
+    Serial.println("DESACTIVAR SENSOR MOVIMIENTO");
+  }
 };
 
 LEDBlinker ledBlinker(13, 300); // Crea un objeto LEDBlinker que parpadea cada 1000 ms
-Alarm alarm(1000);
+Alarm alarma(1000);
 MotionSensor motionSensor(2, 1000);
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
   // Configuración inicial si es necesaria
 }
 
 void loop()
 {
-  ledBlinker.update(); // Actualiza el estado del LED
-  // Aquí puedes actualizar otros objetos que hereden de Updater
-  Serial.println(alarm.getAlarmState());
-  if (motionSensor.getMotionState())
+
+  // Activa la alarma
+  if (!digitalRead(5))
   {
-    alarm.alarm();
+    ledBlinker.changeMode("ON");
+    alarma.activate();
   }
 
-  alarm.update();
+  // Salta el detector de movimiento
+  if (!digitalRead(7))
+  {
+    motionSensor.update();
+  }
+  // Desactiva la alarma
+  if (!digitalRead(6))
+  {
+    ledBlinker.changeMode("OFF");
+    alarma.deactivate();
+  }
+
+  // Serial.println(alarma.getAlarmState());
+  if (motionSensor.getMotionState() && alarma.isActivatedAlarm())
+  {
+    ledBlinker.changeMode("BLINK");
+    alarma.alarm();
+  }
+
+  ledBlinker.update(); // Actualiza el estado del LED
 }
